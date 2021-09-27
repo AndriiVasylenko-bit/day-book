4c0a56fed519fcb2c266583d225446e21832e21c
Andrii <83074078+KIDOS95@users.noreply.github.com>
2021-09-27 21:50:10 +0300

    general accessess

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace DayBook
+{
+
+    class Program
+    {
+        //    Описати клас з іменем STUDENT, який містить наступні поля: прізвище; номер групи; успішність(масив із п’яти елементів).
+        //    Написати програму, яка виконує наступні дії:
+        //    -   зчитування даних з рядкового потоку в масив, який складається структур типу STUDENT.
+        //    -  впорядкувати їх за зростанням номера групи; 
+        //    виведення на дисплей прізвищ і номерів груп для всіх студентів,
+        //    включених до масиву, якщо середній бал студента більший ніж 4.0; 
+        //    якщо таких студентів немає, вивести відповідне повідомлення.
+        //    -  реалізувати пошук інформації по не повному прізвищу
+
+        static void Main(string[] args)
+        {
+            //string nickSurname;
+
+            for (int i = 0; i < 5; i++)
+            {
+                string[] surname = new string[5];
+                {
+                    Console.Write("Введите фамилию: ");
+                    surname[i] = Console.ReadLine();
+                }
+                string[] name = new string[5];
+                {
+                    Console.Write("Введите имя: ");
+                    name[i] = Console.ReadLine();
+                }
+                string[] patronymic = new string[5];
+                {
+                    Console.Write("Введите отчество: ");
+                    patronymic[i] = Console.ReadLine();
+                }
+
+
+                Student student = new Student(surname[i], name[i], patronymic[i]);
+
+                Console.WriteLine($"Студент: {student.FullName}\n");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DayBook
+{
+    class Student
+    {
+        private string studentSurname;
+        private string studentName;
+        private string studentPatronymic;
+
+        public Student(string Surname, string Name, string Patronymic)
+        {
+            StudentSurname = Surname;
+            StudentName = Name;
+            StudentPatronymic = Patronymic;
+        }
+
+        public string FullName
+        {
+            get
+            {
+                return $"{studentSurname} {studentName.Substring(0, 1)}. {studentPatronymic.Substring(0, 1)}.";
+            }
+        }
+
+        public string StudentSurname
+        {
+            get
+            {
+                return studentSurname;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException("Фамилия студента не может быть пустым.");
+                }
+                studentSurname = value;
+            }
+        }
+
+        public string StudentName
+        {
+            get
+            {
+                return studentName;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException("Имя студента не может быть путсым.");
+                }
+                studentName = value;
+            }
+        }
+
+        public string StudentPatronymic
+        {
+            get
+            {
+                return studentPatronymic;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException("Отчество студента не может быть путсым.");
+                }
+                studentPatronymic = value;
+            }
+
+        }
+
+    }
+}

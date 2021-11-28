3a2735beeaf07511a4aae51ba622e46e2df66c34
bLendeR965 <vladzabuvaka@gmail.com>
2021-11-28 12:40:55 +0200

    Added group manager

---
+++ src/DaybookCore/GroupManager.cs
@@ -0,0 +1,40 @@
+﻿using DaybookCore.Entities;
+
+namespace DaybookCore
+{
+    public static class GroupManager
+    {
+        private static readonly List<Group> _groups;
+
+        static GroupManager()
+        {
+            _groups = new List<Group>();
+        }
+
+        public static void AddStudent(long groupId, Student student)
+        {
+            _groups?.FirstOrDefault
+            (
+                group => group.Id == groupId
+            )?.Participants?.Add(student);
+        }
+
+        public static void RemoveStudent(long groupId, Student student)
+        {
+            _groups?.FirstOrDefault
+            (
+                group => group.Id == groupId
+            )?.Participants?.Add(student);
+        }
+
+        public static void AddGroup(Group group)
+        {
+            _groups.Add(group);
+        }
+
+        public static void DeleteGroup(Group group)
+        {
+            _groups.Remove(group);
+        }
+    }
+}

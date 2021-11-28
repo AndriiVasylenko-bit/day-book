74e9920086c94e5975e3356e562dad0b748968c7
bLendeR965 <vladzabuvaka@gmail.com>
2021-11-28 12:39:55 +0200

    added participants

--- src/DaybookCore/Entities/Group.cs
+++ src/DaybookCore/Entities/Group.cs
@@ -3,10 +3,12 @@
     public class Group
     {
         public long Id { get; set; }
+        public List<Student>? Participants { get; set; }
 
-        public Group(long id)
+        public Group(long id, List<Student>? participants)
         {
             Id = id;
+            Participants = participants;
         }
     }
 }
